2718ad2dc9e38ff5e382a355ba55fbc81e0253f3
greplog <devopsbeta@gmail.com>
2020-04-22 11:50:37 -0500

    updated appVar.py

--- flask-youtubeDL/appVar.py
+++ flask-youtubeDL/appVar.py
@@ -1,3 +1,8 @@
+# This is the folder in the container.
+#Do not change without updating docker-compose.yml.
 dlDir = "/data/scratch/"
+# This is the audio file name schema
 fileFormat = "%(title)s.%(ext)s"
+# Flags for youtube-dl - modify to suite your need.
+# This will give you already meta and thumbnail picture in m4a format
 ytdl_flags = "-f bestaudio[ext=m4a] -i --embed-thumbnail --add-metadata"
